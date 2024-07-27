bdd9e129341a7fe58944c45152b1abf94e509b69
qbessi <github@bessi.io>
2024-07-28 01:41:23 +0300

    config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://qbessi.github.io',
-	base: 'main',
+	base: '/',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
